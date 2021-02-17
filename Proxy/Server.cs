9aa1c361fe243c359e400d091d679206a6a5fd07
Your NameMoreno092 <you@example.com>
2021-02-17 11:09:26 +0100

    Add project files.

---
+++ Proxy/Server.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Server: IServer
+    {
+        private string Order;
+        public void TakeOrder(string order)
+        {
+            Console.WriteLine("Server takes order for " + order + ".");
+            Order = order;
+        }
+
+        public string DeliverOrder()
+        {
+            return Order;
+        }
+
+        public void ProcessPayment(string payment)
+        {
+            Console.WriteLine("Payment for order (" + payment + ") processed.");
+        }
+    }
+}
